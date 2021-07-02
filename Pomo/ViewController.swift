c620900bd48ef06479b8ca0f49debffd50b97b7f
xavvvier <xavvvier@users.noreply.github.com>
2021-07-02 09:00:31 -0300

    Initial Commit

---
+++ Pomo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Pomo
+//
+//  Created by Javier on 2021-07-02.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
